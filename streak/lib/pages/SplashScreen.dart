e1fb23c0658d66e458576026ea7a805dc47ce16e
RicMerces <ricardoliveira0703@gmail.com>
2022-04-19 14:47:05 -0300

    SplashScreen pt 01

---
+++ streak/lib/pages/SplashScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Splashscreen extends StatefulWidget {
+  @override
+  _SplashscreenState createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- streak/test/widget_test.dart
+++ streak/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:streak/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(Streak());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
