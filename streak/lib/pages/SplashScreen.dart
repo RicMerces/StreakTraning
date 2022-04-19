e337c43048c0d27fbce8dead9005f146846d924a
RicMerces <ricardoliveira0703@gmail.com>
2022-04-19 14:58:49 -0300

    SplashScreen

--- streak/lib/pages/SplashScreen.dart
+++ streak/lib/pages/SplashScreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Splashscreen extends StatefulWidget {
   @override
@@ -8,6 +8,14 @@ class Splashscreen extends StatefulWidget {
 class _SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xff2C2C2C),
+      body: Center(
+        child: Image.asset(
+          'assets/img/logoStreak.png',
+          height: 300,
+        ),
+      ),
+    );
   }
 }
